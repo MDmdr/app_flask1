3b20d0f00b8aa564f715639ddae54a1b263c1b14
MDmdr <rmd.melgar@gmail.com>
2022-03-24 11:40:50 -0600

    test app falsk1

---
+++ view.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# @app.route('/', methods=['POST', 'GET'])
+# def home():
+#     return render_template('index.html')
+#     return "kdjgjdh"
+    
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(debug=True, host='0.0.0.0', port=port)
